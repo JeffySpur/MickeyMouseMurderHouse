fb1f48e6fa9e960b1a5ce8e6c7979ca6ba50de79
Jeffrey Spurling <89798840+JeffySpur@users.noreply.github.com>
2021-09-28 16:24:07 -0400

    first commit

---
+++ MainRoom/CharacterTemplate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainRoom
+{
+    public class CharacterTemplate
+    {
+        public CharacterTemplate() { }
+        public CharacterTemplate(string name, int age, string role)
+        {
+            Name = name;
+            Age = age;
+            Role = role;
+        }
+       
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Role { get; set; }
+    }
+
+
+}
---
+++ MainRoom/MurderhouseUI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainRoom
+{
+    class MurderhouseUI
+    {
+
+
+
+
+        //helper stuff
+    public void PressAnyKey()
+    {
+        console.WriteLine("press key to continue");
+        console.ReadKey();
+    }
+
+    private void SeedData()
+        {
+            CharacterTemplate daisy = new CharacterTemplate("Daisy", 42, "Child Star");
+            daisy.Testimony = "testmonial things you're gonna right";
+
+            CharacterTemplate donald = new CharacterTemplate("Donald", 52, "Director");
+            donald.Testimony = "testmonial things you're gonna right";
+
+            CharacterTemplate mickey = new CharacterTemplate("Mickey", 90, "Leading Man");
+            mickey.Testimony = "testmonial things you're gonna right";
+
+            CharacterTemplate pete = new CharacterTemplate("Pete", 62, "Chef");
+            pete.Testimony = "testmonial things you're gonna right";
+
+            CharacterTemplate goofy = new CharacterTemplate("Goofy", 65, "Bellhop");
+            goofy.Testimony = "testmonial things you're gonna right";
+
+            CharacterTemplate minnie = new CharacterTemplate("Minnie", 85, "Starlet");
+            minnie.Testimony = "testmonial things you're gonna right";
+
+
+        }
+    }
+
+}
